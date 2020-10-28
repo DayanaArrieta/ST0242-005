82533a2e2ddc07f21a476ec593394f22363542e7
jjquintang <73456606+jjquintang@users.noreply.github.com>
2020-10-28 13:56:43 -0500

    Add files via upload

---
+++ Entrega 1/Código/Versión 1/Principal.java
@@ -0,0 +1,13 @@
+public  class Principal {
+        public Principal() {
+        }
+
+ 
+
+        public static void main(String[] args) {
+            Juego juego = new Juego();
+            juego.jugar();
+        }
+    }
+
+
